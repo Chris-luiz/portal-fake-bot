fbcc8fac45e51bbdb17bfa569ade81d305673ea7
Christian Luiz de Souza Paes <you@example.com>
2026-07-07 16:58:38 -0400

    feat: implementar automação de cadastro

---
+++ cadastro.py
@@ -0,0 +1,7 @@
+def cadastrar():
+    print("Iniciando cadastro...")
+    print("Inserindo dados...")
+    print("Dados inseridos com sucesso...")
+    print("Encerrando operação...")
+
+cadastrar()
